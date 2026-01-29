ed77cca3ba5b4aed16322e7f42eea1d6f718c8f7
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2026-01-29 15:48:58 +0800

    refactor submitReschedulingAppeal parameters

--- frontend/src/app/core/services/faculty/rescheduling/rescheduling.service.ts
+++ frontend/src/app/core/services/faculty/rescheduling/rescheduling.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../../../environments/environment.prod';
+import { environment } from '../../../../../environments/environment.dev';
 import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/internal/operators/catchError';
 import { throwError } from 'rxjs/internal/observable/throwError';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +14,35 @@ export class ReschedulingService {
   constructor(private http: HttpClient) {}
 
   /** * Submits a rescheduling appeal to the backend API.
-   * @param appealData The data related to the rescheduling appeal.
+   * @param scheduleId - The ID of the schedule to be rescheduled.
+   * @param appealFile - The file containing the appeal document.
+   * @param reason - The reason for the rescheduling appeal.
+   * @param appealDetails - An object containing the proposed
+   *  new schedule details (day, start time, end time, room).
+   * @return An Observable representing the HTTP response from the API.
    */
-  submitReschedulingAppeal(appealData: any) {
+  submitReschedulingAppeal(
+    scheduleId: number,
+    appealFile: File | null, // TODO: Change to required when file upload is implemented
+    reason: string,
+    appealDetails: {
+      day: string,
+      startTime: string,
+      endTime: string,
+      room: string
+    }
+  ): Observable<any> {
     const url = `${this.baseUrl}/submit-rescheduling-appeal`;
 
-    return this.http.post(url, appealData).pipe(
+    // TODO: Create checks for the payload data
+    const payload = {
+      scheduleId,
+      appealFile,
+      reason,
+      appealDetails
+    }
+
+    return this.http.post(url, payload).pipe(
       catchError((error) => {
         console.error('Error submitting rescheduling appeal:', error);
         return throwError(() => error);
