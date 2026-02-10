25d525323d3fe89e89c9b69a6e4a25a64cf8dbaf
jundam-dannkyle <jundamdannkyle@gmail.com>
2026-02-10 15:26:47 +0800

    Update environment for Azure production

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000/api',
+  // Point to Azure instead of localhost
+  apiUrl: 'https://flss-backend-api-d9eecxcnhpccdpdk.southeastasia-01.azurewebsites.net/api',
   appUrl: 'http://localhost:4201',
 };
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.pupt-flss.com/api',
-  appUrl: 'https://beta.pupt-flss.com',
+  // 1. YOUR NEW AZURE BACKEND URL (Keep the /api at the end)
+  apiUrl: 'https://flss-backend-api-d9eecxcnhpccdpdk.southeastasia-01.azurewebsites.net/api',
+  
+  // 2. YOUR FUTURE FRONTEND URL
+  // (We don't know this yet because we haven't deployed the frontend. 
+  // For now, put a placeholder or your planned custom domain if you have one.)
+  appUrl: 'https://flss-frontend.azurestaticapps.net', 
 };
