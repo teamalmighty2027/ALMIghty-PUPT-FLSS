8b2cf8f1e30d9a19d3556572541305f459783254
jundam-dannkyle <jundamdannkyle@gmail.com>
2026-02-26 23:13:30 +0800

    Add down method to reverse appeals table migration

--- backend/database/migrations/2026_02_22_145621_create_appeals_table.php
+++ backend/database/migrations/2026_02_22_145621_create_appeals_table.php
@@ -35,4 +35,12 @@ return new class extends Migration
             $table->timestamps();
         });
     }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appeals');
+    }
 };
