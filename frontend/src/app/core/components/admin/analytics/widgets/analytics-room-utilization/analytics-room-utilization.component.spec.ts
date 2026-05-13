4527f7ea6a6481b3fb6ea5fa9a632feb06593b31
bautista-johnmatthew <131367336+bautista-johnmatthew@users.noreply.github.com>
2026-05-13 13:03:22 +0800

    Create data analytics page (#127)
    * create analytics controller
    
    * add analytics endpoints
    
    * install chart js package
    
    * add analytics page to admin routes
    
    * create analytics component
    
    * create analytics service
    
    * fix missing import of MatIconModule
    
    * create analytics-heatmap component
    
    * create analytics-faculty-type component
    
    * move analytics widget directory
    
    * create analytics-room-component
    
    * rearrange analytics heatmap grid postion
    
    * create analytics-faculty-load component
    
    * add empty semesters error logic
    
    * fix faculty load distribution join query
    
    * create analytics-program-coverage component
    
    * fix analytics widgets loading by semester
    
    * fix program-coverage component computation logic
    
    * move data analytics page to overview page

---
+++ frontend/src/app/core/components/admin/analytics/widgets/analytics-room-utilization/analytics-room-utilization.component.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { 
+  AnalyticsRoomUtilizationComponent 
+} from './analytics-room-utilization.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+
+describe('AnalyticsRoomUtilizationComponent', () => {
+  let component: AnalyticsRoomUtilizationComponent;
+  let fixture: ComponentFixture<AnalyticsRoomUtilizationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AnalyticsRoomUtilizationComponent, NoopAnimationsModule],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideCharts(withDefaultRegisterables()),
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AnalyticsRoomUtilizationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
