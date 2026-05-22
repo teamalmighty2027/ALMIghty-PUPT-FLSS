5bdf8cc76d5edfcff06e1155074eb37558c32359
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2026-05-22 13:58:49 +0800

    create eloquent models

---
+++ backend/app/Models/Elective.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Elective extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'elective_id';
+
+    protected $fillable = [
+        'elective_slot_name',
+        'course_code',
+        'course_title',
+        'lec_hours',
+        'lab_hours',
+        'units',
+        'tuition_hours',
+        'description',
+        'is_active',
+    ];
+
+    /**
+     * Get curriculum elective assignments for this elective.
+     */
+    public function curriculumElectives()
+    {
+        return $this->hasMany(
+            CurriculumElective::class,
+            'selected_elective_id',
+            'elective_id'
+        );
+    }
+
+    /**
+     * Get academic year elective assignments for this elective.
+     */
+    public function academicYearElectives()
+    {
+        return $this->hasMany(
+            AcademicYearElective::class,
+            'selected_elective_id',
+            'elective_id'
+        );
+    }
+}
