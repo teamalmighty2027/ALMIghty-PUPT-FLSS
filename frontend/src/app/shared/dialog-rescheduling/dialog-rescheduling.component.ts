374889c5007629d98432826a45994739b911becc
teves-gwen <tevesgwenn.sch@gmail.com>
2026-06-02 09:43:41 +0800

    Revert "redesign scheduling and rescheduling dialogs for polish and consistency"
    This reverts commit b2f2530152ff83a02dc5520a394bd8673e310ad9.

--- frontend/src/app/shared/dialog-rescheduling/dialog-rescheduling.component.ts
+++ frontend/src/app/shared/dialog-rescheduling/dialog-rescheduling.component.ts
@@ -7,21 +7,19 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-rescheduling',
   standalone: true,
   imports: [
-    CommonModule,
-    FormsModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatRippleModule,
+    CommonModule, 
+    FormsModule, 
+    MatDialogModule, 
+    MatFormFieldModule, 
+    MatSelectModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatIconModule
   ],
   templateUrl: './dialog-rescheduling.component.html',
   styleUrl: './dialog-rescheduling.component.scss'
