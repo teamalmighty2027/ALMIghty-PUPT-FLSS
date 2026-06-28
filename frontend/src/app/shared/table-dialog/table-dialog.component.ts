18fdbc9372fe9918e302ab1f82259b289f808249
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2026-06-28 23:40:34 +0800

    apply min length restriction to other fields

--- frontend/src/app/shared/table-dialog/table-dialog.component.ts
+++ frontend/src/app/shared/table-dialog/table-dialog.component.ts
@@ -418,8 +418,16 @@ export class TableDialogComponent {
       (field.formControlName === 'password' ||
         field.formControlName === 'confirmPassword')
     ) {
-      const control = this.fb.control(initialValue);
+      const control = this.fb.control(initialValue, {
+        validators: [Validators.minLength(12)],
+      });
       this.form.addControl(field.formControlName, control);
+      if (field.confirmPassword) {
+        control.setValidators([
+          Validators.minLength(12),
+          this.passwordMatchValidator.bind(this),
+        ]);
+      }
     } else {
       const control = this.fb.control(initialValue, { validators });
       this.form.addControl(field.formControlName, control);
@@ -499,6 +507,7 @@ export class TableDialogComponent {
     if (field.maxLength) validators.push(Validators.maxLength(field.maxLength));
     if (field.minLength) validators.push(Validators.minLength(field.minLength));
     if (field.type === 'text') validators.push(this.noWhitespaceValidator);
+    if (field.type === 'password') validators.push(Validators.minLength(12));
     if (field.type === 'number')
       validators.push(Validators.pattern(/^\d{1,2}$/));
     if (field.min !== undefined) validators.push(Validators.min(field.min));
@@ -563,10 +572,19 @@ export class TableDialogComponent {
       return `${label} cannot exceed ${
         control.getError('maxlength').requiredLength
       } characters.`;
-    if (control.hasError('minlength'))
+    if (control.hasError('minlength')) {
+      if (
+        formControlName === 'password' ||
+        formControlName === 'confirmPassword'
+      ) {
+        return `${label} must be at least ${
+          control.getError('minlength').requiredLength
+        } characters.`;
+      }
       return `${label} must be exactly ${
         control.getError('minlength').requiredLength
       } characters.`;
+    }
     if (control.hasError('pattern')) {
       return control.getError('pattern').requiredPattern === '/^\\d{4}$/'
         ? `${label} must be exactly 4 digits.`
