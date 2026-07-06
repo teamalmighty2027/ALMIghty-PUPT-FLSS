ca4de94c1c95aa65be5565e4011b6e4a89e737be
bautista-johnmatthew <131367336+bautista-johnmatthew@users.noreply.github.com>
2026-07-06 16:18:50 +0800

    Revert htaccess configuration (#213)
    * revert htaccess configuration to hide backend url
    
    * fix htaccess API rewrite rule

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  // AZURE BACKEND URL
+  apiUrl: 'https://test-flss.alquatrilixbsit2027.com/api',
+  
+  // AZURE FRONTEND URL
   appUrl: 'https://test-flss.alquatrilixbsit2027.com/', 
 };
